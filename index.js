f555fd477876dea5a994ded14dc0f68e26bf784f
Umar-Saif56 <umarsaif4917@gmail.com>
2025-07-23 11:48:21 +0500

    nodemon changr

--- index.js
+++ index.js
@@ -25,6 +25,6 @@ mongoose
 
 // Railway dynamic port handling
 const port = process.env.PORT || 3000;
-app.listen(port, "0.0.0.0", () => {
+app.listen(port, "0.0.0.0",  () => {
   console.log(`Server connected to port ${port}`);
 });
